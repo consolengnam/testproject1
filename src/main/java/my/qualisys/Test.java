da71594069283247e226136725890622763c5054
consolengnam@gmail.com <Console NOUMEDEM>
2022-02-08 00:18:45 +0100

    correctif bug

--- src/main/java/my/qualisys/Test.java
+++ src/main/java/my/qualisys/Test.java
@@ -1,7 +1,10 @@
 package my.qualisys;
 
 public class Test {
+	/*
+	  nouveau commentaire du dev 2
 	
+	*/
 	public static void main(String[] args){
 		//ceci est un comentaire
 	  System.out.println("Hello!");
